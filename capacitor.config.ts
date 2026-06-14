0f1c6c6fd04efa841393a69d22a48ad07403b190
alvin358 <alvin@bonanza-factory.co.kr>
2026-06-14 17:06:17 +0900

    chore: add Capacitor and Android platform support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alvintracer.cotext',
+  appName: 'cotext',
+  webDir: 'dist'
+};
+
+export default config;

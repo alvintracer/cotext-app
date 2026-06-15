fc56b307979a12256cf1214aff7c6e76b10dcf75
alvin358 <alvin@bonanza-factory.co.kr>
2026-06-16 08:30:48 +0900

    fix: add safe-area insets for Android status/nav bar overlap

--- android/app/src/main/java/com/alvintracer/cotext/MainActivity.java
+++ android/app/src/main/java/com/alvintracer/cotext/MainActivity.java
@@ -1,5 +1,14 @@
 package com.alvintracer.cotext;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Enable edge-to-edge: WebView extends behind status bar & nav bar
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}

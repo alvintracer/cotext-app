6d0756e48861b74a51ee0cd1dffca1d113001806
alvin358 <alvin@bonanza-factory.co.kr>
2026-06-16 09:31:34 +0900

    fix: revert edge-to-edge, use Android default system bar handling

--- android/app/src/main/java/com/alvintracer/cotext/MainActivity.java
+++ android/app/src/main/java/com/alvintracer/cotext/MainActivity.java
@@ -1,14 +1,5 @@
 package com.alvintracer.cotext;
 
-import android.os.Bundle;
-import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Enable edge-to-edge: WebView extends behind status bar & nav bar
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-    }
-}
+public class MainActivity extends BridgeActivity {}
